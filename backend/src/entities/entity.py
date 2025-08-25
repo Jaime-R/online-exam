10aafccd2cef972472579d9d3da68ba0988bef7e
Jaime-R <jaime_rios.1xyz@yahoo.com>
2025-08-25 06:01:22 -0600

    Correccion de errores SQLAlquemy and some others

---
+++ backend/src/entities/entity.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+db_url = 'localhost:5432'
+db_name = 'online_exam_db'
+db_user = 'online_exam_user'
+db_password = '0NLIN3-ex4m'
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class Entity():
+	id = Column(Integer, primary_key=True)
+	created_at = Column(DateTime)
+	update_at = Column(DateTime)
+	last_update_by = Column(String)
+
+	def __init__(self, created_by):
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
+		self.last_updated_by = created_by
